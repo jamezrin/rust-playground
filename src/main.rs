4b8b78c82139fe9c503aa56f2e1e9ab3eb9b6853
Jaime Martínez Rincón <jaime@jamezrin.name>
2024-06-20 20:47:08 +0200

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use std::{env, fs, thread};
+use std::env::join_paths;
+use std::fmt::format;
+use std::io::{BufWriter, Error, Write};
+
+const THING: u32 = 0xf09f8c88;
+
+const THING_COUNT: u64 = u64::pow(10, 8);
+
+const THING_FILE_CHUNKS: u32 = 5;
+
+fn write_chunk(out_path: &std::path::Path) -> Result<(), Error> {
+    let file = fs::File::create(out_path)?;
+    let mut w = BufWriter::new(&file);
+
+    for _ in 0..THING_COUNT {
+        let thing_bytes = &(THING.to_be_bytes());
+        w.write_all(thing_bytes)?;
+    }
+
+    w.flush().expect("Flushing buffer failed");
+
+    let metadata = file.metadata()?;
+    println!(
+        "File '{}' written, size: {} MB",
+        out_path.file_name().unwrap().to_str().unwrap(),
+        metadata.len() / (1024 * 1024)
+    );
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let out_path = env::current_dir()?
+        .join("out");
+
+    if fs::remove_dir_all(&out_path).is_ok() {
+        println!("Deleted output directory");
+    }
+
+    fs::create_dir_all(&out_path)
+        .expect("Failed to create output directory");
+
+    println!("Writing {} file chunks", THING_FILE_CHUNKS);
+    thread::scope(|scope| {
+        for i in 0..THING_FILE_CHUNKS {
+            let chunk_name = format!("chunk-{}", i);
+            let file_path = out_path.clone().join(chunk_name.clone());
+
+            scope.spawn(move || {
+                match write_chunk(&file_path) {
+                    Ok(_) => println!("Successfully wrote chunk {}", chunk_name),
+                    Err(_) => eprintln!("Failed to write chunk {}", chunk_name)
+                }
+            });
+        }
+    });
+
+    Ok(())
+}
